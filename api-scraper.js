b42bff1a58ed1669c07d9726d04d0b540138b3b3
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-09-21 11:10:30 -0400

    Update api-scraper.js

--- api-scraper.js
+++ api-scraper.js
@@ -1,13 +1,15 @@
-// requires NodeJS
+// Requires NodeJS
 
-const request = require("request");
+var request = require("request"); // request module import
+var EventEmitter = require("events").EventEmitter; // live update as data comes in from API @ https://data.cityofnewyork.us/resource/fjn5-bxwg.json?incident_zip=10458
+var body = new EventEmitter(); // create new emitter for body
 
-function getData() {
-  return request("https://data.cityofnewyork.us/resource/fjn5-bxwg.json?incident_zip=10458", function (error, response, body) {
-    return JSON.parse(body);
-  })
-}
 
-var data = getData();
+request("https://data.cityofnewyork.us/resource/fjn5-bxwg.json?incident_zip=10458", function(error, response, data) {
+    body.data = JSON.parse(data); // parse json
+    body.emit('update'); // continue stream of body data
+});
 
-console.log(data)
+body.on('update', function () {
+    console.log(body.data);
+});
