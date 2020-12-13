56205e87dfc6fd34021197249662b3f9220f7dd3
JakeHack <71288657+JakeHack@users.noreply.github.com>
2020-12-13 14:49:22 -0500

    Add files via upload

--- comments.php
+++ comments.php
@@ -12,9 +12,6 @@ $username = "kthaokar";
 $password = "kthaokar!";
 $dbName = "CISC2500";
 
-echo "Connect Succe";
-print "Test Test";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbName);
 
@@ -22,26 +19,21 @@ $conn = new mysqli($servername, $username, $password, $dbName);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 
 $name = filter_input(INPUT_POST, 'name');
 $email = filter_input(INPUT_POST, 'email');
 $comment = filter_input(INPUT_POST, 'comments');
-
-echo $name;
 
 $sql = "INSERT INTO Cyber_Comments (name, email, comment) VALUES ('$name', '$email','$comment')";
 
 /*$sql = "INSERT INTO account (username, password)
 values ('$FirstName','$LastName','$SchoolName','$Address','$Major')";*/
 
-echo $sql;
-
-if ($conn->query($sql) === TRUE) {
+/*if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
-}
+}*/
 
 $conn->close();
 ?>
